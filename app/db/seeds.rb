fd90ae9876c9209563fc901821f0faebf3a32455
JUANA <estidy@gmail.com>
2023-09-05 23:49:47 -0300

    datos de prueba en la tabla productos (100)

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.destroy_all
+
+100.times do |index|
+  Product.create!(title: Faker::Name.name,
+                description: Faker::Lorem.paragraph,
+                price: Faker::Number.number(digits: 4))
+end
