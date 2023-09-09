1cd8e0c4fe90cdc0e9905105c77d0e32d6dc7feb
JUANA <estidy@gmail.com>
2023-09-09 00:14:32 -0300

    #1 agregando metodo delete

--- app/app/controllers/products_controller.rb
+++ app/app/controllers/products_controller.rb
@@ -36,6 +36,10 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy
+    redirect_to products_path, notice: 'El producto se ha eliminado exitosamente', status: :see_other
   end
 
   private
