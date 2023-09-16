487c96fa31efa2f19a6e6aedd24b87f1dd144e8e
JUANA <estidy@gmail.com>
2023-09-15 22:14:49 -0300

    #4 agregando categorias de prueba a la base de datos

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -7,9 +7,23 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Product.destroy_all
+Category.destroy_all
 
 100.times do |index|
   Product.create!(title: Faker::Name.name,
                 description: Faker::Lorem.paragraph,
                 price: Faker::Number.number(digits: 4))
+end
+
+categories = [
+             "Panaderia",
+             "Cafeteria",
+             "Sandwiches",
+             "Entradas",
+             "Bebidas Sin Alcohol",
+             "Plato Principal"
+            ]
+            
+categories.each do |category|
+     Category.create!(name: category)
 end
