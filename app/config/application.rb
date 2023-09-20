eb7238510d93bd9b3fd6b1732746bbdc146a5ebe
JUANA <estidy@gmail.com>
2023-09-19 22:56:49 -0300

    #6 configurando las consultas async

--- app/config/application.rb
+++ app/config/application.rb
@@ -18,5 +18,9 @@ module Docker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Allow multiquery
+    config.active_record.async_query_executor = :global_thread_pool
+    
   end
 end
