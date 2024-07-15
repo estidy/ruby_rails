84ac708e0b1b9b9be4885adeea3616b1ad8e670e
JUANA <estidy@gmail.com>
2024-07-15 19:06:17 -0300

    definiendo la ruta para la autenticacion

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :categories, except: :show
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :products
   root to: "products#index"
   
+  namespace :authentication do 
+    resources :users, only: [:new, :create]
+  end
+
 end
